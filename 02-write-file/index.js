f1335daaa19edf58983f797cfece4a496164d7fd
Vitaliy Yarosh <iarosch@protonmail.com>
2022-05-21 18:39:31 +0300

    feat: add resolve for task 02, (Sat, May 21 2022, 18:39 PM)

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,11 +1,21 @@
 const fs = require('fs');
 const path = require('path');
+const { stdin: input, stdout: output } = require('process');
+const readline = require('readline');
+const rl = readline.createInterface({input, output});
 
-fs.writeFile(
-  path.join(__dirname, 'text.txt'),
-  '',
-  (err) => {
-    if (err) throw err;
-    console.log('Файл был создан');
+let writeableStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+output.write('Введите текст: \n');
+
+rl.on('line', (input) => {
+  if(input === 'exit') {
+    output.write('Всего доброго! Процесс завершен');
+    rl.close();
   }
-);
+  writeableStream.write(`${input}\n`);
+});
+
+rl.on('SIGINT', () => {
+  output.write('Всего доброго! Процесс завершен');
+  rl.close();
+});
