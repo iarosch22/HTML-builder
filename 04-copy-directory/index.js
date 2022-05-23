f577464b87486dd25346c0f337524acb7afd504e
Vitaliy Yarosh <iarosch@protonmail.com>
2022-05-23 12:29:02 +0300

    feat: add resolve for 04-copy-directory task, (Mon May 23 2022, 12:28)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+const destination = path.join(__dirname, 'files-copy');
+const source = path.join(__dirname, 'files');
+
+async function copyDir(destination, source) {
+  await fs.mkdir(destination, {recursive: true});
+  const files = await fs.readdir(source, {withFileTypes: true});
+  files.forEach(async file => {
+    await fs.copyFile(path.join(source, file.name), path.join(destination, file.name));
+  });
+}
+
+copyDir(destination, source);
