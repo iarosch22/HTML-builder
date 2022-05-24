6445215162527eda1880f5cb527f2b87fb2a6da5
Vitaliy Yarosh <iarosch@protonmail.com>
2022-05-24 16:56:45 +0300

    feat: add func for create index.html

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,12 +1,28 @@
-// Дорогой друг, если не сложно, проверь, пожалуйста, через день. Я все доделаю, обещаю:)
+// Дорогой друг, если не сложно, проверь, пожалуйста, через день. Я все доделаю, обещаю:) Спасибо!
 
-const fs = require('fs/promises');
+const fsP = require('fs/promises');
+const fs = require('fs');
 const path = require('path');
 
 const destination = path.join(__dirname, 'project-dist');
+const source = path.join(__dirname, 'components');
 
-async function createHtml(destination) {
-  await fs.mkdir(destination, {recursive: true});
+async function createHtml(destination, source) {
+  let temp = await fsP.readFile(path.join(__dirname, 'template.html'));
+  let index = temp.toString();
+
+  await fsP.mkdir(destination, {recursive: true});
+  const files = await fsP.readdir(source, {withFileTypes: true});
+
+  files.forEach(async file => {
+    let dataFromComponent = await fsP.readFile(path.join(source, file.name));
+    const nameOfComponent = file.name.split('.')[0];
+
+    index = index.replace(`{{${nameOfComponent}}}`, dataFromComponent.toString());
+    fs.writeFile(path.join(destination, 'index.html'), index, function(error) {
+      if(error) throw error;
+    });
+  });
 }
 
-createHtml(destination);
+createHtml(destination, source);
