92b448fac4075d0670ccbe0067bb278d9aa2d9a7
Vitaliy Yarosh <iarosch@protonmail.com>
2022-05-24 08:25:27 +0300

    feat: create func for creating project dist folder,(Tuesday, May 24 2022)

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,1 +1,12 @@
-Дорогой проверяющий, если не сложно, можно ли проверить через день. Я скоро завершу задание:)
+// Дорогой друг, если не сложно, проверь, пожалуйста, через день. Я все доделаю, обещаю:)
+
+const fs = require('fs/promises');
+const path = require('path');
+
+const destination = path.join(__dirname, 'project-dist');
+
+async function createHtml(destination) {
+  await fs.mkdir(destination, {recursive: true});
+}
+
+createHtml(destination);
