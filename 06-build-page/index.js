e9a3a9a75bdc57ddc1eb5b27181fbe94d7379d4a
Vitaliy Yarosh <iarosch@protonmail.com>
2022-05-25 08:31:34 +0300

    fix: some fixies for 06 task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -8,23 +8,23 @@ const destination = path.join(__dirname, 'project-dist');
 const source = path.join(__dirname, 'components');
 const sourceStyles = path.join(__dirname, 'styles');
 
-async function createHtml(destination, source) {
+(async () => {
   let temp = await fsP.readFile(path.join(__dirname, 'template.html'));
   let index = temp.toString();
-
+  
   await fsP.mkdir(destination, {recursive: true});
   const files = await fsP.readdir(source, {withFileTypes: true});
-
+  
   files.forEach(async file => {
     let dataFromComponent = await fsP.readFile(path.join(source, file.name));
     const nameOfComponent = file.name.split('.')[0];
-
+  
     index = index.replace(`{{${nameOfComponent}}}`, dataFromComponent.toString());
     fs.writeFile(path.join(destination, 'index.html'), index, function(error) {
       if(error) throw error;
     });
   });
-}
+})();
 
 (async () => {
   let writeableStream = fs.createWriteStream(path.join(destination, 'style.css'));
@@ -40,4 +40,10 @@ async function createHtml(destination, source) {
     }
   });
 })();
-createHtml(destination, source);
+
+// const destinationAssets = path.join(__dirname, 'project-dist', 'assets');
+// const sourceAssets = path.join(__dirname, 'assets');
+
+// (async () => {
+//   fs.mkdir(destinationAssets, {recursive: true});
+// })();
